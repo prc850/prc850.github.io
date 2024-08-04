66eb096d66ad439fa607ac7aa1d7472c3e83671a
prc850 <prc850@aol.com>
2024-08-04 01:26:31 -0500

    Update request-order.php

--- request-order.php
+++ request-order.php
@@ -1,6 +1,28 @@
 <!DOCTYPE html>
 <html lang="en-US">
   <?php
-    echo"I like pizza<br>";
+    // define variables and set to empty values
+$nameErr = $emailErr = $genderErr = $websiteErr = "";
+$name = $email = $gender = $comment = $website = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  if (empty($_POST["name"])) {
+    $nameErr = "Name is required";
+  } else {
+    $name = test_input($_POST["name"]);
+  }
+
+  if (empty($_POST["emailornumb"])) {
+    $emailornumbErr = "Email is required";
+  } else {
+    $email = test_input($_POST["email"]);
+  }
+  if (empty($_POST["comment"])) {
+    $comment = "";
+  } else {
+    $comment = test_input($_POST["comment"]);
+  }
+}
+
   ?>
 </html>
